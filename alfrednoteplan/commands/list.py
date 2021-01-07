2e0c7f29e8bf2b7d25fbedfd00fa8c1ad3b85660
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-01-07 20:07:15 +0900

    Naive way to show first line

--- alfrednoteplan/commands/list.py
+++ alfrednoteplan/commands/list.py
@@ -1,14 +1,13 @@
-
 from alfrednoteplan import decorators, constants
-
 
 
 @decorators.jsonfilter
 def main():
     for path in constants.NOTES.glob("*.md"):
-        yield {
-            "arg": "noteplan://x-callback-url/openNote?fileName=" + path.name,
-            "title": path.stem,
-            "autocomplete": path.stem,
-            "subtitle": str(path),
-        }
+        with path.open() as fp:
+            yield {
+                "arg": "noteplan://x-callback-url/openNote?fileName=" + path.name,
+                "title": path.stem,
+                "autocomplete": path.stem,
+                "subtitle": fp.readline(),
+            }
